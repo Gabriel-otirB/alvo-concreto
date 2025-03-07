03e173b108a4562f2243abae3238bd3474a188db
Gabriel-otirB <lordglayder@gmail.com>
2025-03-07 20:53:54 -0300

    Footer CSS improved

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -10,95 +10,90 @@ export const FooterArea = styled.footer`
     padding: 40px 0;
     background-size: cover; 
     background-position: bottom;
-}
 
-.footer-content h4 {
+    h4 {
     font-size: 18px;
     font-weight: bold;
     margin-bottom: 20px;
     color: var(--dark-gray-color);
     text-transform: uppercase;
     text-align: center;
-}
+    }
 
-.footer-contact,
-.footer-socials {
+    i {
+    color: var(--red-color);
+    margin-right: 5px;
+        }
+    }
+
+    p {
+    font-weight: bold;
+    font-size: 16px;
+    margin-top: 20px;
+    color: var(--footer-dev-gray);
+    }
+
+    .footer-contact,
+    .footer-socials {
     list-style: none;
     padding: 0;
     text-align: center;
     margin: 0 auto;
-}
+    }
 
-.footer-contact li,
-.footer-socials li {
+    .footer-contact li,
+    .footer-socials li {
     font-size: 16px;
     font-weight: bold;
     margin-bottom: 10px;
     color: var(--dark-gray-color);
-}
 
-.footer-socials i {
+    .dev a {
     color: var(--red-color);
-    margin-right: 5px;
-}
+    font-weight: bold;
+    text-decoration: none;
+    transition: color 0.5s ease;
+    margin-right: 4px;
 
-.footer-contact i {
-    color: var(--red-color);
-    margin-right: 5px;
+    &:hover {
+    color: var(--dark-gray-color);
+    text-decoration: none;
+    }
+}
 }
 
 .footer-contact li {
     display: flex;
     align-items: center;
     justify-content: center;
+
+    a {
+    color: var(--dark-gray-color);
+    text-decoration: none;
+    font-size: 16px;
+    transition: color 0.5s ease;
+
+    &:hover {
+    color: var(--red-color);
+        }
+    }
 }
 
 .footer-socials li {
     display: inline-block;
     margin: 0 15px;
-}
 
-.footer-socials a {
+    a {
     color: var(--dark-gray-color);
     text-decoration: none;
     font-size: 16px;
     font-weight: bold;
     transition: color 0.5s ease;
-}
 
-.footer-socials a:hover {
+    &:hover {
     color: var(--red-color);
-}
-
-.footer-contact a {
-    color: var(--dark-gray-color);
-    text-decoration: none;
-    font-size: 16px;
-    transition: color 0.5s ease;
-}
-
-.footer-contact a:hover {
-    color: var(--red-color);
-}
-
-.footer-content .text-center p {
-    font-weight: bold;
-    font-size: 16px;
-    margin-top: 20px;
-    color: var(--footer-dev-gray);
-}
-
-.footer-content .dev a {
-    color: var(--red-color);
-    font-weight: bold;
-    text-decoration: none;
-    transition: color 0.5s ease;
-    margin-right: 4px;
-}
-
-.footer-content .dev a:hover {
-    color: var(--dark-gray-color);
-    text-decoration: none;
+        }
+    }
 }
 
 `
