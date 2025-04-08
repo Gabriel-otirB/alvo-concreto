2c32d3cd4acdb33d35b14feea0d2eb78a89cdafb
Gabriel-otirB <lordglayder@gmail.com>
2025-04-07 21:25:16 -0300

    refacture: Extracting data from services

---
+++ src/utils/data.js
@@ -0,0 +1,114 @@
+export const servicesList = {
+  delivery: {
+    firstService: {
+      title: 'Entrega rápida e confiável.',
+      icon: 'fa-solid fa-truck',
+    },
+    secondService: {
+      title: 'Concreto de alta qualidade para diferentes aplicações.',
+      icon: 'fa-solid fa-cogs'
+    },
+    thirdService: {
+      title: 'Equipe dedicada e atendimento personalizado.',
+      icon: 'fa-solid fa-handshake'
+    },
+  },
+  consultancy: {
+    firstService: {
+      title: 'Consultoria técnica especializada para otimização de processos.',
+      icon: 'fa-solid fa-headset'
+    },
+    secondService: {
+      title: 'Análise e diagnóstico técnico detalhado para melhorias operacionais.',
+      icon: 'fa-solid fa-cogs'
+    },
+    thirdService: {
+      title: 'Certificação de conformidade e qualidade após implementação das soluções.',
+      icon: 'fa-solid fa-certificate'
+    },
+  },
+  planing: {
+    firstService: {
+      title: 'Planejamento estratégico para execução eficiente de obras.',
+      icon: 'fa-solid fa-calendar-alt'
+    },
+    secondService: {
+      title: 'Análise de viabilidade e gestão de cronogramas de obras.',
+      icon: 'fa-solid fa-chart-line'
+    },
+    thirdService: {
+      title: 'Certificação de conformidade com normas e qualidade no planejamento.',
+      icon: 'fa-solid fa-certificate'
+    },
+  },
+  customize: {
+    firstService: {
+      title: 'Criação de projetos exclusivos, adaptados às necessidades do cliente.',
+      icon: 'fa-solid fa-palette'
+    },
+    secondService: {
+      title: 'Integração de tecnologia e inovação nos projetos.',
+      icon: 'fa-solid fa-cogs'
+    },
+    thirdService: {
+      title: 'Garantia de qualidade e atendimento a todas as normas técnicas.',
+      icon: 'fa-solid fa-check-circle'
+    },
+  },
+  quality: {
+    firstService: {
+      title: 'Garantia de execução conforme os padrões técnicos e regulatórios.',
+      icon: 'fa-solid fa-shield-alt'
+    },
+    secondService: {
+      title: 'Controle de qualidade rigoroso em todas as etapas do projeto.',
+      icon: 'fa-solid fa-check-circle'
+    },
+    thirdService: {
+      title: 'Materiais e técnicas de construção de alta durabilidade e resistência.',
+      icon: 'fa-solid fa-thumbs-up'
+    },
+  },
+  sustainability: {
+    firstService: {
+      title: 'Soluções sustentáveis para redução de impacto ambiental.',
+      icon: 'fa-solid fa-leaf'
+    },
+    secondService: {
+      title: 'Implementação de práticas ecoeficientes em processos e operações.',
+      icon: 'fa-solid fa-recycle'
+    },
+    thirdService: {
+      title: 'Certificação de conformidade com normas ambientais e sustentabilidade.',
+      icon: 'fa-solid fa-sun'
+    },
+  },
+  maintenance: {
+    firstService: {
+      title: 'Manutenção preventiva e corretiva de equipamentos.',
+      icon: 'fa-solid fa-wrench'
+    },
+    secondService: {
+      title: 'Capacitação especializada para técnicos em manutenção.',
+      icon: 'fa-solid fa-tools'
+    },
+    thirdService: {
+      title: 'Certificação para os profissionais após conclusão dos treinamentos.',
+      icon: 'fa-solid fa-certificate'
+    },
+  },
+  training: {
+    firstService: {
+      title: 'Treinamentos especializados para todas as etapas da obra.',
+      icon: 'fa-solid fa-chalkboard-teacher'
+    },
+    secondService: {
+      title: 'Capacitação em segurança e operação de equipamentos.',
+      icon: 'fa-solid fa-users-cog'
+    },
+    thirdService: {
+      title: 'Certificação para os participantes após a conclusão dos cursos.',
+      icon: 'fa-solid fa-graduation-cap'
+    },
+  },
+}
